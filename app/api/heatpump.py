bbd4cfde89ba6ddf37e02b10a6e614f58b9a6989
Andreas Schmid <service@aaschmid.de>
2024-01-14 21:38:17 +0100

    return all heatpump values with single endpoint and remove duplication
    Signed-off-by: Andreas Schmid <service@aaschmid.de>

--- app/api/heatpump.py
+++ app/api/heatpump.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from PyViCare import PyViCareDeviceConfig, PyViCareHeatPump
-from PyViCare.PyViCare import PyViCare
+from PyViCare.PyViCareHeatPump import Compressor
 
 from app import dependencies
 
@@ -8,42 +8,45 @@ ROUTE_PREFIX_HEATPUMP = "/heatpump"
 router = APIRouter(prefix=ROUTE_PREFIX_HEATPUMP)
 
 
-def get_single_heatpump_device(vicare: PyViCare = Depends(dependencies.get_vicare)) -> PyViCareDeviceConfig:
-    result = [d for d in vicare.devices if "type:heatpump" in d.service.roles]
-    if len(result) <= 0:
-        raise HTTPException(422, "No heatpump device found.")
-    if len(result) > 1:
-        raise HTTPException(422, "Multiple heatpump devices found, currently unsupported.")
-    return result[0]
+def get_single_heatpump(device: PyViCareDeviceConfig = Depends(get_single_heating_device)) -> PyViCareHeatPump:
+    return device.asHeatPump()
 
 
-def get_single_heatpump(vicare: PyViCare = Depends(dependencies.get_vicare)) -> PyViCareHeatPump:
-    return get_single_heatpump_device(vicare).asHeatPump()
+def get_single_compressor(heatpump: PyViCareHeatPump = Depends(get_single_heatpump)) -> Compressor:
+    if len(heatpump.compressors) <= 0:
+        raise HTTPException(422, "No compressor found for heatpump.")
+    if len(heatpump.compressors) > 1:
+        raise HTTPException(422, "Multiple compressors found, currently unsupported.")
+    return heatpump.compressors[0]
 
 
 @router.get("/")
-def get_heatpump(device: PyViCareDeviceConfig = Depends(get_single_heatpump_device)) -> dict:
+def get_heatpump(
+    device: PyViCareDeviceConfig = Depends(get_single_heating_device),
+    heatpump: PyViCareHeatPump = Depends(get_single_heatpump),
+    compressor: Compressor = Depends(get_single_compressor),
+) -> dict:
     return {
-        "deviceId": device.device_id,
-        "model": device.device_model,
-        "serial": device.service.accessor.serial,
+        "device": {
+            "boilerSerial": heatpump.getBoilerSerial(),
+            "controllerSerial": heatpump.getControllerSerial(),
+            "deviceId": device.device_id,
+            "model": device.device_model,
+            "serial": device.service.accessor.serial,
+        },
+        "compressor": {
+            "active": compressor.getActive(),
+            "hours": compressor.getHours(),
+            "phase": compressor.getPhase(),
+            "starts": compressor.getStarts(),
+        },
+        "errors": device.service.getProperty("device.messages.errors.raw")["properties"]["entries"]["value"],
+        "temperature": {
+            "buffer": heatpump.getBufferMainTemperature(),
+            "outside": heatpump.getOutsideTemperature(),
+            "primaryCircuitSupply": heatpump.getSupplyTemperaturePrimaryCircuit(),
+            "return": heatpump.getReturnTemperature(),
+            "secondaryCircuitSupply": heatpump.getSupplyTemperatureSecondaryCircuit(),
+        },
         "status": device.status,
     }
-
-
-@router.get("/temperature")
-def get_temperature(heatpump: PyViCareHeatPump = Depends(get_single_heatpump)) -> dict:
-    return {
-        "outside": heatpump.getOutsideTemperature(),
-        "return": heatpump.getReturnTemperature(),
-    }
-
-
-@router.get("/temperature/outside")
-def get_temperature_outside(heatpump: PyViCareHeatPump = Depends(get_single_heatpump)) -> float:
-    return heatpump.getOutsideTemperature()
-
-
-@router.get("/temperature/return")
-def get_temperature_return(heatpump: PyViCareHeatPump = Depends(get_single_heatpump)) -> float:
-    return heatpump.getReturnTemperature()
--- tests/api/test_heatpump.py
+++ tests/api/test_heatpump.py
@@ -2,7 +2,6 @@ from unittest.mock import Mock
 
 import pytest
 from fastapi.testclient import TestClient
-from PyViCare.PyViCareDeviceConfig import PyViCareDeviceConfig
 
 from app.api.heatpump import ROUTE_PREFIX_HEATPUMP
 from app.main import app
@@ -12,36 +11,63 @@ client = TestClient(app)
 
 @pytest.mark.parametrize("dependency_mocker", [app], indirect=True)
 def test_heatpump_should_return_meta_information_on_root(dependency_mocker):
-    dependency_mocker.devices = [
-        PyViCareDeviceConfig(
-            Mock(roles=["type:heatpump"], accessor=Mock(serial="test_serial")), 1234, "test_device", "online"
-        )
-    ]
-
-    response = client.get(ROUTE_PREFIX_HEATPUMP)
-
-    assert response.status_code == 200
-    assert response.json() == {
-        "deviceId": 1234,
-        "model": "test_device",
-        "serial": "test_serial",
-        "status": "online",
+    property_map = {
+        "device.messages.errors.raw": {"properties": {"entries": {"value": ["error x"]}}},
     }
-
-
-@pytest.mark.parametrize("dependency_mocker", [app], indirect=True)
-def test_heatpump_temperature(dependency_mocker):
     dependency_mocker.devices = [
         Mock(
-            service=Mock(roles=["type:heatpump"]),
-            asHeatPump=lambda: Mock(getOutsideTemperature=lambda: 3.3, getReturnTemperature=lambda: 4.4),
+            asHeatPump=lambda: Mock(
+                compressors=[
+                    Mock(
+                        getActive=lambda: True,
+                        getHours=lambda: 100,
+                        getPhase=lambda: "?",
+                        getStarts=lambda: 12,
+                    )
+                ],
+                getBoilerSerial=lambda: "boiler-serial",
+                getBufferMainTemperature=lambda: 21.3,
+                getControllerSerial=lambda: "controller-serial",
+                getOutsideTemperature=lambda: 3.3,
+                getSupplyTemperaturePrimaryCircuit=lambda: 29,
+                getSupplyTemperatureSecondaryCircuit=lambda: 24.7,
+                getReturnTemperature=lambda: 4.4,
+            ),
+            device_id=1234,
+            device_model="test_device",
+            service=Mock(
+                roles=["type:heatpump"],
+                accessor=Mock(serial="test_serial"),
+                getProperty=lambda p: property_map[p],
+            ),
+            status="online",
         )
     ]
-    response = client.get(f"{ROUTE_PREFIX_HEATPUMP}/temperature")
+
+    response = client.get(ROUTE_PREFIX_HEATING_HEATPUMP)
 
     assert response.status_code == 200
-
     assert response.json() == {
-        "outside": 3.3,
-        "return": 4.4,
+        "device": {
+            "boilerSerial": "boiler-serial",
+            "controllerSerial": "controller-serial",
+            "deviceId": 1234,
+            "model": "test_device",
+            "serial": "test_serial",
+        },
+        "compressor": {
+            "active": True,
+            "hours": 100,
+            "phase": "?",
+            "starts": 12,
+        },
+        "errors": ["error x"],
+        "temperature": {
+            "buffer": 21.3,
+            "outside": 3.3,
+            "primaryCircuitSupply": 29,
+            "return": 4.4,
+            "secondaryCircuitSupply": 24.7,
+        },
+        "status": "online",
     }
