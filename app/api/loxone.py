2171f0519bd68d8e203b5a58445dca0df6c97c88
Andreas Schmid <service@aaschmid.de>
2024-09-29 21:55:42 +0200

    use mikrotik fetch and its data capability to get status
    Signed-off-by: Andreas Schmid <service@aaschmid.de>

--- app/api/loxone.py
+++ app/api/loxone.py
@@ -1,8 +1,7 @@
 import re
-from typing import Annotated
 
 import requests
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException
 from requests.auth import HTTPBasicAuth
 from starlette import status
 
@@ -15,12 +14,8 @@ router = APIRouter(prefix=ROUTE_PREFIX_LOXONE)
 CODE_PATTERN = re.compile(r'Code="(\d+)"')
 
 
-# Not really restful but required for Mikrotik's netwatch HTTP probe, see https://help.mikrotik.com/docs/display/ROS/Netwatch
-@router.get("/mode/sleep/{state}", status_code=status.HTTP_204_NO_CONTENT)
-def get_mode_sleep(
-    state: Annotated[str, Path(title="The state for which a HTTP-200 should be answered", pattern=r"^on|off$")],
-    settings: Settings = Depends(dependencies.get_settings),
-) -> None:
+@router.get("/mode/sleep/state", status_code=status.HTTP_200_OK)
+def get_mode_sleep(settings: Settings = Depends(dependencies.get_settings)) -> bool:
     data = requests.get(
         f"{settings.loxone_url}/dev/sps/io/1b60daf4-0071-17ba-ffffed57184a04d2",
         auth=(HTTPBasicAuth(settings.loxone_user, settings.loxone_password)),
@@ -32,7 +27,4 @@ def get_mode_sleep(
         if real_status_code != 200:
             raise HTTPException(real_status_code, detail=f"Loxone answered with HTTP {real_status_code}")
 
-    if f'value="{0 if state == "off" else 1}"' in data:
-        return None
-
-    raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return True if 'value="1"' in data else False
--- tests/api/test_loxone.py
+++ tests/api/test_loxone.py
@@ -14,7 +14,7 @@ client = TestClient(app)
 @pytest.mark.parametrize("dependency_mocker", [app], indirect=True)
 def test_get_mode_sleep_invalid_state(dependency_mocker):
     response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/invalid")
-    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
 @pytest.mark.parametrize(
@@ -25,7 +25,7 @@ def test_get_mode_sleep_invalid_state(dependency_mocker):
 def test_get_mode_sleep_should_call_correct_api(dependency_mocker, mocker):
     mock = mocker.patch("app.api.loxone.requests.get", return_value=(Mock(text='<LL Code="200"/>')))
 
-    client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/on")
+    client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/state")
 
     assert mock.call_count == 1
     assert mock.call_args.args[0] == "https://localhost:8080/dev/sps/io/1b60daf4-0071-17ba-ffffed57184a04d2"
@@ -37,26 +37,26 @@ def test_get_mode_sleep_should_call_correct_api(dependency_mocker, mocker):
 def test_get_mode_sleep_should_forward_non_200_from_loxone(dependency_mocker, mocker):
     mocker.patch("app.api.loxone.requests.get", return_value=(Mock(text='<LL control="foobar" value="0" Code="403"/>')))
 
-    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/off")
+    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/state")
 
     assert response.status_code == 403
 
 
 @pytest.mark.parametrize(
-    "dependency_mocker, state, value, expected",
+    "dependency_mocker, value, expected_json, expected_str",
     [
-        (app, "on", 0, status.HTTP_404_NOT_FOUND),
-        (app, "on", 1, status.HTTP_204_NO_CONTENT),
-        (app, "off", 0, status.HTTP_204_NO_CONTENT),
-        (app, "off", 1, status.HTTP_404_NOT_FOUND),
+        (app, 0, False, "false"),
+        (app, 1, True, "true"),
     ],
     indirect=["dependency_mocker"],
 )
-def test_get_mode_sleep(dependency_mocker, mocker, state, value, expected):
+def test_get_mode_sleep(dependency_mocker, mocker, value, expected_json, expected_str):
     mocker.patch(
         "app.api.loxone.requests.get", return_value=(Mock(text=f'<LL control="foobar" value="{value}" Code="200"/>'))
     )
 
-    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/{state}")
+    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/state")
 
-    assert response.status_code == expected
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == expected_json
+    assert response.text == expected_str
