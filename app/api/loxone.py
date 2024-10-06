961af84bf3c402b26f236dcbb0e449f3fdec83f4
Andreas Schmid <service@aaschmid.de>
2024-10-06 11:47:31 +0200

    use (independently programmable) wifi state
    instead of sleep mode in order be more flexible with putting logic
    behind turning on and off wifi in Mikrotik infrastructure
    
    Signed-off-by: Andreas Schmid <service@aaschmid.de>

--- app/api/loxone.py
+++ app/api/loxone.py
@@ -14,10 +14,10 @@ router = APIRouter(prefix=ROUTE_PREFIX_LOXONE)
 CODE_PATTERN = re.compile(r'Code="(\d+)"')
 
 
-@router.get("/mode/sleep/state", status_code=status.HTTP_200_OK)
-def get_mode_sleep(settings: Settings = Depends(dependencies.get_settings)) -> bool:
+@router.get("/mode/wifi/state", status_code=status.HTTP_200_OK)
+def get_mode_wifi(settings: Settings = Depends(dependencies.get_settings)) -> bool:
     data = requests.get(
-        f"{settings.loxone_url}/dev/sps/io/1b60daf4-0071-17ba-ffffed57184a04d2",
+        f"{settings.loxone_url}/dev/sps/io/1da6432d-00ed-6dfa-ffffed57184a04d2",
         auth=(HTTPBasicAuth(settings.loxone_user, settings.loxone_password)),
         verify=True,
     ).text
--- tests/api/test_loxone.py
+++ tests/api/test_loxone.py
@@ -12,8 +12,8 @@ client = TestClient(app)
 
 
 @pytest.mark.parametrize("dependency_mocker", [app], indirect=True)
-def test_get_mode_sleep_invalid_state(dependency_mocker):
-    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/invalid")
+def test_get_mode_wifi_invalid_state(dependency_mocker):
+    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/wifi/invalid")
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
 
@@ -22,22 +22,22 @@ def test_get_mode_sleep_invalid_state(dependency_mocker):
     [[app, {"loxone_url": "https://localhost:8080", "loxone_user": "reader", "loxone_password": "password1"}]],
     indirect=True,
 )
-def test_get_mode_sleep_should_call_correct_api(dependency_mocker, mocker):
+def test_get_mode_wifi_should_call_correct_api(dependency_mocker, mocker):
     mock = mocker.patch("app.api.loxone.requests.get", return_value=(Mock(text='<LL Code="200"/>')))
 
-    client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/state")
+    client.get(f"{ROUTE_PREFIX_LOXONE}/mode/wifi/state")
 
     assert mock.call_count == 1
-    assert mock.call_args.args[0] == "https://localhost:8080/dev/sps/io/1b60daf4-0071-17ba-ffffed57184a04d2"
+    assert mock.call_args.args[0] == "https://localhost:8080/dev/sps/io/1da6432d-00ed-6dfa-ffffed57184a04d2"
     assert mock.call_args.kwargs["auth"] == HTTPBasicAuth("reader", "password1")
     assert mock.call_args.kwargs["verify"]
 
 
 @pytest.mark.parametrize("dependency_mocker", [app], indirect=True)
-def test_get_mode_sleep_should_forward_non_200_from_loxone(dependency_mocker, mocker):
+def test_get_mode_wifi_should_forward_non_200_from_loxone(dependency_mocker, mocker):
     mocker.patch("app.api.loxone.requests.get", return_value=(Mock(text='<LL control="foobar" value="0" Code="403"/>')))
 
-    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/state")
+    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/wifi/state")
 
     assert response.status_code == 403
 
@@ -50,12 +50,12 @@ def test_get_mode_sleep_should_forward_non_200_from_loxone(dependency_mocker, mo
     ],
     indirect=["dependency_mocker"],
 )
-def test_get_mode_sleep(dependency_mocker, mocker, value, expected_json, expected_str):
+def test_get_mode_wifi(dependency_mocker, mocker, value, expected_json, expected_str):
     mocker.patch(
         "app.api.loxone.requests.get", return_value=(Mock(text=f'<LL control="foobar" value="{value}" Code="200"/>'))
     )
 
-    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/sleep/state")
+    response = client.get(f"{ROUTE_PREFIX_LOXONE}/mode/wifi/state")
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == expected_json
